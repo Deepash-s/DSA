b7c6e0c8a50f3eb534de2213f4b8ab9aec06d18d
Deepash-s <deepashs18@gmail.com>
2026-05-03 14:30:45 +0530

    bubble sort

---
+++ Sorting/bubble.py
@@ -0,0 +1,13 @@
+arr = [64, 25, 12, 22, 11]
+n = len(arr)
+
+for i in range(n-1):
+    swapped = False
+    for j in range(n-i-1):
+        if(arr[j]>arr[j+1]):
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            swapped = True
+    if swapped == False:
+        break
+    
+print(arr)
