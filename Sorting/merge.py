568fe53ee08f6058899d10a680e5d872806d5270
Deepash-s <deepashs18@gmail.com>
2026-05-18 00:02:55 +0530

    merge sort

---
+++ Sorting/merge.py
@@ -0,0 +1,36 @@
+arr = [3, 2, 4, 1, 3]
+
+def mergesort(arr, low, high):
+    if (low >= high):
+        return
+    mid = (low + high) // 2
+    mergesort(arr, low, mid)
+    mergesort(arr, mid+1, high)
+    merge(arr, low, mid, high)
+    
+def merge(arr, low, mid, high):
+    temp = []
+    left = low
+    right = mid+1
+    while(left <= mid and right <= high):
+        if(arr[left] <= arr[right]):
+            temp.append(arr[left])
+            left += 1
+        else:
+            temp.append(arr[right])
+            right += 1
+    
+    while(left <= mid):
+        temp.append(arr[left])
+        left += 1
+    
+    while(right <= high):
+        temp.append(arr[right])
+        right += 1
+        
+    for i in range(low, high+1):
+        arr[i] = temp[i-low]
+            
+mergesort(arr, 0, 4)
+
+print(arr)
